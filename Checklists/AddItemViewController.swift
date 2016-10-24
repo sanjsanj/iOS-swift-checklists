feb6330aafe7ac91a8df4db279e94d99c03fefe2
Sanjay Purswani <sanjsanj@hotmail.com>
2016-10-24 19:59:44 +0100

    Wire up text field as a delegate to add item controller

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -9,7 +9,9 @@
 import Foundation
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -29,5 +31,15 @@ class AddItemViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    @objc(textField:shouldChangeCharactersInRange:replacementString:) func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneBarButton.isEnabled = (newText.length > 0)
+        
+        return true
     }
 }
